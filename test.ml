f1f6de7ce52a923ff9fb967081d546dc006fd075
Kate <kit.ty.kate@disroot.org>
2018-05-09 17:09:49 +0100

    Basic countdown example using javascript functions

--- test.ml
+++ test.ml
@@ -1,2 +1,17 @@
+open Lwt.Infix
+
 let () =
-  print_endline "Hello there \\o"
+  Lwt_js_events.async begin fun () ->
+    print_endline "Hello there \\o";
+    let elt = Dom_html.getElementById_exn "test" in
+    let content = elt##.innerHTML in
+    Lwt_js.sleep 1. >>= fun () ->
+    let content = content##concat (Js.string "..2") in
+    elt##.innerHTML := content;
+    Lwt_js.sleep 1. >>= fun () ->
+    let content = content##concat (Js.string "..1") in
+    elt##.innerHTML := content;
+    Lwt_js.sleep 1. >>= fun () ->
+    elt##.innerHTML := Js.string "Hi \\o";
+    Lwt.return ()
+  end
